d24c43940b12d7749a4a03e57c99bbfe0427934f
Juank <juanchitonup@gmail.com>
2025-04-01 02:19:15 -0500

    API-REST-Payments SpringBoot Version

---
+++ BikeRent-API/src/main/java/BikeRent/Payment/PaymentRepository.java
@@ -0,0 +1,9 @@
+package BikeRent.Payment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, String>{
+
+}
