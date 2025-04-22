ba6da34818b9eb238f87abe7a8c7bd9ac145b9e5
Juan Sebastián Cabrera Bolaños <juansabas2021@udenar.edu.co>
2025-04-22 02:07:40 -0500

    API-REST SpringBoot Version2. Integración de Keycloak

--- BikeRent-API/src/main/java/BikeRent/Location/LocationController.java
+++ BikeRent-API/src/main/java/BikeRent/Location/LocationController.java
@@ -2,6 +2,7 @@ package BikeRent.Location;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,21 +16,25 @@ public class LocationController {
     private LocationRepository locationRepository;
 
     @GetMapping
+    @PreAuthorize("hasRole('user_client_role') or hasRole('admin_client_role')")
     public List<Location> getAllLocations() {
         return locationRepository.findAll();
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('user_client_role') or hasRole('admin_client_role')")
     public Location getLocationById(@PathVariable String id){
         return locationRepository.findById(id).get();
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('admin_client_role')")
     public Location createLocation(@RequestBody Location location){
         return locationRepository.save(location);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('admin_client_role')")
     public Location deleteLocation(@PathVariable String id){
         if(locationRepository.existsById(id)){
             locationRepository.deleteById(id);
@@ -38,6 +43,7 @@ public class LocationController {
     }
 
     @PutMapping
+    @PreAuthorize("hasRole('admin_client_role')")
     public ResponseEntity<Location> updateLocation(@RequestBody Location updateLocation) {
         Optional<Location> optionalLocation = locationRepository.findById(updateLocation.getId());
         if (optionalLocation.isPresent()) {
